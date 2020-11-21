dc040cb0e61159b5dd16fde9f12488d31383e697
Kaitlyn Dang <kaitlyndang@kaitlyns-mbp-2.wireless-1x.unc.edu>
2020-11-21 17:11:33 -0500

    fixed img, fixed html and js for animations

--- javascript/animations.js
+++ javascript/animations.js
@@ -51,3 +51,20 @@ let targets = document.querySelectorAll("body > *");
 targets.forEach((target) => {
   observer.observe(target);
 });
+
+// button
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
